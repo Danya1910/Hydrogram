fb09608849b4e9d6e8e305de69bcba3695ff605a
Danya1910 <shirniy.tonny@gmail.com>
2026-07-01 20:26:21 +0300

    initial repository (save profile + get by Id + set online stats)

---
+++ app/src/main/java/com/example/hydrogram/domain/repository/UserRepository.kt
@@ -0,0 +1,14 @@
+package com.example.hydrogram.domain.repository
+
+import com.example.hydrogram.domain.model.User
+import kotlinx.coroutines.flow.Flow
+
+interface UserRepository {
+
+    suspend fun saveUserProfile(user: User) : Result<Unit>
+
+    fun getUserById(uid: String) : Flow<User?>
+
+    suspend fun setUserOnlineStats(uid: String, isOnline: Boolean) : Result<Unit>
+
+}
