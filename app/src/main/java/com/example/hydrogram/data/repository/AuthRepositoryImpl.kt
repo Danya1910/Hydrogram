0f41101017747ce24f56cbaf319329e2614ceaa3
Danya1910 <shirniy.tonny@gmail.com>
2026-07-02 21:26:56 +0300

    add email + createdAt for user data

---
+++ app/src/main/java/com/example/hydrogram/data/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,4 @@
+package com.example.hydrogram.data.repository
+
+class AuthRepositoryImpl {
+}
--- app/src/main/java/com/example/hydrogram/domain/model/User.kt
+++ app/src/main/java/com/example/hydrogram/domain/model/User.kt
@@ -4,5 +4,7 @@ data class User(
     val uid: String = "",
     val name: String = "",
     val avatarUrl: String = "",
+    val email: String = "",
     val isOnline: Boolean = false,
+    val createdAt: Long = 0L,
 )
