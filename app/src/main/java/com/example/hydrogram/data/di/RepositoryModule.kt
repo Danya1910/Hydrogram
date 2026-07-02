3637883d7bec63ee6bcaca70c88788227619c896
Danya1910 <shirniy.tonny@gmail.com>
2026-07-02 19:58:39 +0300

    add modules

--- app/src/main/java/com/example/hydrogram/data/di/RepositoryModule.kt
+++ app/src/main/java/com/example/hydrogram/data/di/RepositoryModule.kt
@@ -1,4 +1,35 @@
 package com.example.hydrogram.data.di
 
-object RepositoryModule {
+import com.example.hydrogram.domain.repository.ChatRepository
+import com.example.hydrogram.domain.repository.InboxRepository
+import com.example.hydrogram.domain.repository.UserRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindChatRepository(
+        chatRepository: ChatRepository,
+    ) : ChatRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindInboxRepository(
+        inboxRepository: InboxRepository,
+    ) : InboxRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindUserRepository(
+        userRepository: UserRepository,
+    ) : UserRepository
+
 }
