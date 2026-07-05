dfbc15729219a3113ac26a78a06d265ba6afbde1
Danya1910 <shirniy.tonny@gmail.com>
2026-07-05 23:28:37 +0300

    create user class in usecase (domain layer)

---
+++ app/src/main/java/com/example/hydrogram/presentation/states/UserState.kt
@@ -0,0 +1,4 @@
+package com.example.hydrogram.presentation.states
+
+class UserState {
+}
---
+++ app/src/main/java/com/example/hydrogram/presentation/viewModel/UserViewModel.kt
@@ -0,0 +1,81 @@
+package com.example.hydrogram.presentation.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hydrogram.domain.model.User
+import com.example.hydrogram.domain.usecase.GetUserByIdUseCase
+import com.example.hydrogram.domain.usecase.SaveUserProfileUseCase
+import com.example.hydrogram.domain.usecase.SetUserOnlineStatsUseCase
+import com.example.hydrogram.presentation.states.UserState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class UserViewModel @Inject constructor(
+    private val getUserByIdUseCase: GetUserByIdUseCase,
+    private val saveUserProfileUseCase: SaveUserProfileUseCase,
+    private val setUserOnlineStatsUseCase: SetUserOnlineStatsUseCase,
+    uid: String,
+) : ViewModel() {
+
+    private val _userState = MutableStateFlow<UserState>(UserState.Loading)
+    val userState = _userState.asStateFlow()
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading = _isLoading.asStateFlow()
+
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    val errorMessage = _errorMessage.asStateFlow()
+
+    private val _isSuccess = MutableStateFlow(false)
+    val isSuccess = _isSuccess.asStateFlow()
+
+    init {
+        observeUser(uid = uid)
+    }
+
+    private fun observeUser(
+        uid: String,
+    ) {
+        if (uid.isBlank()) {
+            _userState.value = UserState.Error("Пользователь не найден")
+            return
+        }
+        viewModelScope.launch {
+            getUserByIdUseCase(uid = uid)
+                .catch { exception ->
+                    _userState.value = UserState.Error(
+                        exception.localizedMessage ?: "Ошибка получения данных пользователя"
+                    )
+                }
+                .collect { user ->
+                    _userState.value = UserState.Success(user = user)
+                }
+        }
+
+    }
+
+    fun saveProfile(
+        uid: String,
+        name: String,
+        avatarUrl: String,
+        email: String,
+        isOnline: Boolean,
+        createdAt: Long,
+    ) {
+        if(uid.isBlank()) {
+            _errorMessage.value = "Пользователь не найден"
+            return
+        }
+        viewModelScope.launch {
+            _isLoading.value = true
+            val result = saveUserProfileUseCase(
+
+            )
+            _isLoading.value = false
+        }
+    }
+
+}
