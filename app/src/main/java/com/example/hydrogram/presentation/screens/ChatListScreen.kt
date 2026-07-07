e3a810afeb0f3fb694a05337dcccbddeb6d9974d
Danya1910 <shirniy.tonny@gmail.com>
2026-07-07 16:33:47 +0300

    combine user + chat in one state

--- app/src/main/java/com/example/hydrogram/presentation/screens/ChatListScreen.kt
+++ app/src/main/java/com/example/hydrogram/presentation/screens/ChatListScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.hydrogram.domain.model.Chat
+import com.example.hydrogram.domain.model.User
+import com.example.hydrogram.presentation.states.ChatAndUserUiState
 import com.example.hydrogram.presentation.widgets.ChatItem
 import com.example.hydrogram.presentation.widgets.DividingLine
 import java.util.UUID
@@ -23,84 +25,69 @@ class ChatListScreen {
 fun ChatListScreenPreview() {
 
     val currentTimestamp = System.currentTimeMillis()
-    val previewChats = listOf<Chat>(
+    val previewChats = listOf(
         Chat(
             senderId = "user_pavel_durov",
             chatId = UUID.randomUUID().toString(),
             lastMessage = "Проверь новые обновления безопасности, код отправлен на ревью.",
-            lastMessageType = "TEXT",
-            lastMessageSenderId = "user_pavel_durov",
-            lastMessageTimestamp = currentTimestamp,
-            unreadCount = 3// Только что
+            lastMessageTimestamp = currentTimestamp
         ),
         Chat(
             senderId = "user_android_dev",
             chatId = UUID.randomUUID().toString(),
-            lastMessage = "Я перевел список чатов на LazyColumn, теперь все скроллится просто идеально! 🚀",
-            lastMessageType = "TEXT",
-            lastMessageSenderId = "my_current_user_id", // Отправлено нами
-            lastMessageTimestamp = currentTimestamp - 5 * 60 * 1000, // 5 минут назад
-            unreadCount = 4,
-        ),
-        Chat(
-            senderId = "user_mom",
-            chatId = UUID.randomUUID().toString(),
-            lastMessage = "Покушал? Шапку надел?",
-            lastMessageType = "TEXT",
-            lastMessageSenderId = "user_mom",
-            lastMessageTimestamp = currentTimestamp - 45 * 60 * 1000 // 45 минут назад
-        ),
-        Chat(
-            senderId = "channel_hydrogram_news",
-            chatId = UUID.randomUUID().toString(),
-            lastMessage = "🔥 Релиз новой версии Hydrogram 1.0! Скачивайте и тестируйте прямо сейчас.",
-            lastMessageType = "TEXT",
-            lastMessageSenderId = "channel_hydrogram_news",
-            lastMessageTimestamp = currentTimestamp - 3 * 60 * 60 * 1000 // 3 часа назад
-        ),
-        Chat(
-            senderId = "user_designer_artem",
-            chatId = UUID.randomUUID().toString(),
-            lastMessage = "[Голосовое сообщение]",
-            lastMessageType = "VOICE",
-            lastMessageSenderId = "user_designer_artem",
-            lastMessageTimestamp = currentTimestamp - 24 * 60 * 60 * 1000 // Вчера в это же время
-        ),
-        Chat(
-            senderId = "user_work_group",
-            chatId = UUID.randomUUID().toString(),
-            lastMessage = "Иван: Созвон по багам переносим на пятницу на 14:00.",
-            lastMessageType = "TEXT",
-            lastMessageSenderId = "user_ivan_colleague",
-            lastMessageTimestamp = currentTimestamp - 2 * 24 * 60 * 60 * 1000 // 2 дня назад
+            lastMessage = "Я перевел список чатов на LazyColumn! 🚀",
+            lastMessageTimestamp = currentTimestamp - 5 * 60 * 1000,
+            unreadCount = 4
         )
     )
 
+    // 2. Создаем тестовых пользователей, сопоставленных с ID из чатов
+    val previewUsers = mapOf(
+        "user_pavel_durov" to User(
+            uid = "user_pavel_durov",
+            name = "Павел Дуров",
+            avatarUrl = "https://example.com"
+        ),
+        "user_android_dev" to User(
+            uid = "user_android_dev",
+            name = "Дмитрий (Android)",
+            avatarUrl = ""
+        )
+    )
+
+    // 3. Маппим (объединяем) их в List<ChatUiState>
+    val uiStates = previewChats.map { chat ->
+        ChatAndUserUiState(
+            chat = chat,
+            user = previewUsers[chat.senderId],
+        )
+    }
+
     ChatsList(
-        chats = previewChats
+        uiState = uiStates
     )
 }
 
 
 @Composable
 private fun ChatsList(
-    chats: List<Chat>
+    uiState: List<ChatAndUserUiState>
 ) {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
     ) {
         itemsIndexed(
-            items = chats,
-            key = { _, chat -> chat.chatId }
-        ) { index, chat ->
+            items = uiState,
+            key = { _, state -> state.chat.chatId }
+        ) { index, state ->
 
             ChatItem(
-                chat = chat,
-                user = null,
+                chat = state.chat,
+                user = state.user,
             )
 
-            if (index != chats.size - 1) {
+            if (index != uiState.size - 1) {
                 DividingLine(
                     modifier = Modifier
                         .fillMaxWidth()
