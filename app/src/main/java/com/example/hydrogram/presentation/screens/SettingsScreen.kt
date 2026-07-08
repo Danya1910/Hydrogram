a2c24278d088e0500d758f9b2c4cd92891b1f931
Danya1910 <shirniy.tonny@gmail.com>
2026-07-08 15:27:33 +0300

    init screen

--- app/src/main/java/com/example/hydrogram/presentation/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/hydrogram/presentation/screens/SettingsScreen.kt
@@ -11,23 +11,31 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.internal.composableLambdaInstance
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.hydrogram.R
+import com.example.hydrogram.domain.model.User
+import com.example.hydrogram.presentation.util.MenuRowItem
+import com.example.hydrogram.presentation.widgets.SeparatorLine
 import com.example.hydrogram.ui.theme.Blue
+import com.example.hydrogram.ui.theme.LightGrayBackground
 import com.example.hydrogram.ui.theme.SfProText
 
 
@@ -39,6 +47,30 @@ fun SettingsScreen() {
 
 @Composable
 private fun SettingsScreenPreview() {
+
+    val user = User(
+        name = "Dinosaur"
+    )
+
+    val profileList = listOf(
+        MenuRowItem(
+            title = "Set Emoji Status",
+            icon = R.drawable.ic_bottom_search,
+            onClick = {},
+        ),
+        MenuRowItem(
+            title = "Set Emoji Status",
+            icon = R.drawable.ic_settings,
+            onClick = {},
+        ),
+        MenuRowItem(
+            title = "Set Emoji Status",
+            icon = R.drawable.ic_contacts,
+            onClick = {},
+        ),
+    )
+
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -46,61 +78,77 @@ private fun SettingsScreenPreview() {
                 color = Color.White,
             )
     ) {
+        UserInfoHat(user = user)
         Box(
             modifier = Modifier
-                .weight(0.25f)
                 .background(
-                    color = Blue
+                    color = LightGrayBackground,
                 )
         ) {
-            Text(
-                text = "GLKFKGDKFJGKLJFD",
-                fontFamily = SfProText,
-                fontSize = 34.sp,
-                fontWeight = FontWeight.Bold,
-            )
             Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(
-                        horizontal = 16.dp,
-                    )
+                    .padding(horizontal = 16.dp)
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                ) {
-                    GlassButton(icon = R.drawable.ic_qr)
-                    Spacer(modifier = Modifier.weight(1f))
-                    GlassButton(text = "Edit")
-                }
-                Icon(
-                    painter = painterResource(R.drawable.ic_avatar),
-                    contentDescription = null,
-                    tint = Color.Unspecified,
-                    modifier = Modifier
-                        .size(104.dp)
-                        .clip(shape = CircleShape)
-                )
-                Spacer(modifier = Modifier.height(10.dp))
-                Text(
-                    text = "Dinosaur",
-                    fontFamily = SfProText,
-                    fontSize = 28.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                )
-                Spacer(modifier = Modifier.height(5.dp))
-                Text(
-                    text = "+888 888 888 • @dino",
-                    fontFamily = SfProText,
-                    fontSize = 20.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Medium,
+                MenuRow(
+                    items = profileList
                 )
             }
+        }
+    }
+}
+
+@Composable
+private fun UserInfoHat(
+    user: User?
+) {
+    Box(
+        modifier = Modifier
+            .background(
+                color = Blue
+            )
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(
+                    horizontal = 16.dp,
+                )
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                GlassButton(icon = R.drawable.ic_qr)
+                Spacer(modifier = Modifier.weight(1f))
+                GlassButton(text = "Edit")
+            }
+            Icon(
+                painter = painterResource(R.drawable.ic_avatar),
+                contentDescription = null,
+                tint = Color.Unspecified,
+                modifier = Modifier
+                    .size(104.dp)
+                    .clip(shape = CircleShape)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = user?.name ?: "Unknown",
+                fontFamily = SfProText,
+                fontSize = 28.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                letterSpacing = 0.38.sp
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(
+                text = "+888 888 888 • @dino",
+                fontFamily = SfProText,
+                fontSize = 20.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Medium,
+            )
+            Spacer(modifier = Modifier.height(5.dp))
         }
     }
 }
@@ -110,7 +158,7 @@ private fun GlassButton(
     icon: Int? = null,
     text: String? = null,
 ) {
-    
+
     val glassBrush = Brush.linearGradient(
         colors = listOf(
             Color.White.copy(alpha = 0.55f),
@@ -157,5 +205,66 @@ private fun GlassButton(
                 tint = Color.Black,
             )
         }
+    }
+}
+
+@Composable
+private fun MenuRow(
+    items: List<MenuRowItem>,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(
+                shape = RoundedCornerShape(26.dp)
+            )
+    ) {
+        items.forEachIndexed { index, item ->
+            MenuRowItem(item = item)
+            if (index != items.size - 1) {
+                SeparatorLine(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun MenuRowItem(
+    item: MenuRowItem,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .height(52.dp)
+            .fillMaxWidth()
+            .background(
+                color = Color.White,
+            )
+            .padding(horizontal = 20.dp)
+    ) {
+        Icon(
+            painter = painterResource(item.icon),
+            contentDescription = null,
+            tint = Color.Unspecified,
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = item.title,
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Medium,
+            fontSize = 22.sp,
+            color = Color.Black,
+            letterSpacing = (-0.43).sp
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Icon(
+            painter = painterResource(R.drawable.ic_arrow_right),
+            contentDescription = null,
+            tint = Color.Unspecified,
+        )
     }
 }
