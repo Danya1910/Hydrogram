8ef0ebb390e217213e101153374f3cf2e82cd455
Danya1910 <shirniy.tonny@gmail.com>
2026-07-12 00:50:12 +0500

    add gradient for glass effect

---
+++ app/src/main/java/com/example/hydrogram/presentation/util/ColorStyle.kt
@@ -0,0 +1,19 @@
+package com.example.hydrogram.presentation.util
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+
+val GlassBackground = Brush.linearGradient(
+    colors = listOf(
+        Color.White.copy(alpha = 0.55f),
+        Color.White.copy(alpha = 0.25f),
+    ),
+)
+
+val BorderBrush = Brush.linearGradient(
+    colors = listOf(
+        Color.White.copy(alpha = 0.65f),
+        Color.White.copy(alpha = 0.10f),
+    )
+)
