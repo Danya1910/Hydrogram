4028081fa94f0d4183c44cd92c96d4bd747296bb
Danya1910 <shirniy.tonny@gmail.com>
2026-07-12 13:19:40 +0500

    add color to gradient

--- app/src/main/java/com/example/hydrogram/presentation/util/ColorStyle.kt
+++ app/src/main/java/com/example/hydrogram/presentation/util/ColorStyle.kt
@@ -8,12 +8,14 @@ val GlassBackground = Brush.linearGradient(
     colors = listOf(
         Color.White.copy(alpha = 0.55f),
         Color.White.copy(alpha = 0.25f),
+        Color.White.copy(alpha = 0.45f),
     ),
 )
 
 val GlassBorder = Brush.linearGradient(
     colors = listOf(
-        Color.White.copy(alpha = 0.65f),
-        Color.White.copy(alpha = 0.10f),
+        Color.White.copy(alpha = 0.55f),
+        Color.White.copy(alpha = 0.20f),
+        Color.White.copy(alpha = 0.40f),
     )
 )
