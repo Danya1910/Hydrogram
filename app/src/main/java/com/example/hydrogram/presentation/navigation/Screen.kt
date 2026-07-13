0347d266232834514726247161e042fd5a48ddd4
Danya1910 <shirniy.tonny@gmail.com>
2026-07-13 20:53:12 +0500

    add screens routes

---
+++ app/src/main/java/com/example/hydrogram/presentation/navigation/Screen.kt
@@ -0,0 +1,32 @@
+package com.example.hydrogram.presentation.navigation
+
+sealed interface Screen {
+
+    val route: String
+
+    data object PhoneRegistration : Screen {
+        override val route = "PhoneRegistration"
+    }
+
+    data object EmailRegistration : Screen {
+        override val route = "EmailRegistration"
+    }
+
+    data object PasswordInput : Screen {
+        override val route = "PasswordInput"
+    }
+
+    data object ChatList : Screen {
+        override val route = "ChatList"
+    }
+
+    data object Settings : Screen {
+        override val route = "Settings"
+    }
+
+    data object Chat : Screen {
+        override val route = "Chat"
+    }
+
+
+}
