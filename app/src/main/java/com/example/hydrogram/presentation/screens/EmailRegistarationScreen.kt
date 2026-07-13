3df96bb341d4529b31b71e0730fb967d77ab62c3
Danya1910 <shirniy.tonny@gmail.com>
2026-07-13 14:31:41 +0500

    init email putting screen

---
+++ app/src/main/java/com/example/hydrogram/presentation/screens/EmailRegistarationScreen.kt
@@ -0,0 +1,203 @@
+package com.example.hydrogram.presentation.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.hydrogram.R
+import com.example.hydrogram.presentation.widgets.SeparatorLine
+import com.example.hydrogram.ui.theme.Blue
+import com.example.hydrogram.ui.theme.Separator
+import com.example.hydrogram.ui.theme.SfProText
+
+
+@Composable
+fun EmailRegistrationScreen() {
+    Scaffold(
+        topBar = {},
+    ) { paddingValues ->
+        Content(
+            paddingValues = paddingValues,
+        )
+    }
+}
+
+@Composable
+private fun Content(
+    paddingValues: PaddingValues,
+) {
+
+    var email by remember { mutableStateOf("") }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues = paddingValues)
+            .padding(
+                horizontal = 16.dp,
+                vertical = 26.dp,
+            ),
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_avatar),
+                contentDescription = null,
+                tint = Color.Unspecified,
+                modifier = Modifier
+                    .size(100.dp),
+            )
+            Spacer(modifier = Modifier.height(26.dp))
+            Text(
+                text = "Электронная почта",
+                fontWeight = FontWeight.Bold,
+                fontFamily = SfProText,
+                fontSize = 26.sp,
+                color = Color.Black,
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                textAlign = TextAlign.Center,
+                text = "Вы будете получать коды для входа" +
+                        " в Telegram на адрес электронной почты," +
+                        " а не через SMS. Пожалуйста, введите адрес," +
+                        " к которому у Вас есть доступ.",
+                fontWeight = FontWeight.Normal,
+                fontFamily = SfProText,
+                fontSize = 15.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .fillMaxWidth(0.7f)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            InputEmailField(
+                email = email,
+                onValueChange = {},
+            )
+        }
+
+        AcceptButton(
+            isAvailable = false,
+            onClick = {},
+        )
+    }
+}
+
+@Composable
+private fun InputEmailField(
+    email: String,
+    onValueChange: (String) -> Unit,
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        BasicTextField(
+            value = email,
+            onValueChange = onValueChange,
+            textStyle = TextStyle(
+                fontFamily = SfProText,
+                fontSize = 17.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color.Black
+            ),
+            modifier = Modifier
+                .padding(horizontal = 16.dp),
+            decorationBox = { innerTextField ->
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                    ) {
+                        if (email.isEmpty()) {
+                            Text(
+                                text = "Введите адрес почты",
+                                fontFamily = SfProText,
+                                fontSize = 17.sp,
+                                color = Color.Gray.copy(alpha = 0.8f)
+                            )
+                        }
+                        innerTextField()
+                    }
+            }
+        )
+        SeparatorLine(
+            modifier = Modifier
+                .padding(top = 15.dp)
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+private fun AcceptButton(
+    isAvailable: Boolean,
+    onClick: () -> Unit,
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(44.dp)
+            .background(
+                color = if (isAvailable) Blue else Separator,
+                shape = CircleShape
+            )
+            .border(
+                color = if (isAvailable) Blue else Separator,
+                width = 1.dp,
+                shape = CircleShape,
+            )
+    ) {
+        Text(
+            text = "Применить",
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Medium,
+            fontSize = 17.sp,
+            color = if (isAvailable) Color.White else Color.Black,
+        )
+    }
+
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun PhoneRegistrationScreenPreview() {
+    EmailRegistrationScreen()
+}
