e828dcf857c9bb475746889c7a5fd6eaee1a99ff
Danya1910 <shirniy.tonny@gmail.com>
2026-07-14 11:56:00 +0500

    init screen to insert name

---
+++ app/src/main/java/com/example/hydrogram/presentation/screens/NameInputScreen.kt
@@ -0,0 +1,231 @@
+package com.example.hydrogram.presentation.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.hydrogram.R
+import com.example.hydrogram.presentation.navigation.Screen
+import com.example.hydrogram.presentation.viewModel.AuthViewModel
+import com.example.hydrogram.presentation.widgets.SeparatorLine
+import com.example.hydrogram.ui.theme.Blue
+import com.example.hydrogram.ui.theme.Separator
+import com.example.hydrogram.ui.theme.SfProText
+
+
+@Composable
+fun NameInputScreen(
+    authViewModel: AuthViewModel,
+    navController: NavController,
+) {
+    Scaffold(
+        topBar = {},
+    ) { paddingValues ->
+        Content(
+            authViewModel = authViewModel,
+            navController = navController,
+            paddingValues = paddingValues,
+        )
+    }
+}
+
+@Composable
+private fun Content(
+    authViewModel: AuthViewModel,
+    navController: NavController,
+    paddingValues: PaddingValues,
+) {
+
+    var name by remember { mutableStateOf("") }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues = paddingValues)
+            .padding(
+                horizontal = 16.dp,
+                vertical = 26.dp,
+            ),
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.mailbox),
+                contentDescription = null,
+                tint = Color.Unspecified,
+                modifier = Modifier
+                    .size(200.dp),
+            )
+            Spacer(modifier = Modifier.height(26.dp))
+            Text(
+                text = "Ваш профиль",
+                fontWeight = FontWeight.Bold,
+                fontFamily = SfProText,
+                fontSize = 26.sp,
+                color = Color.Black,
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                textAlign = TextAlign.Center,
+                text = "Пожалуйста, введите ваше настоящее имя" +
+                        " и добавьте фото профиля",
+                fontWeight = FontWeight.Normal,
+                fontFamily = SfProText,
+                fontSize = 15.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .fillMaxWidth(0.7f)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            InputNameField(
+                name = name,
+                onValueChange = {
+                    name = it
+                },
+            )
+        }
+
+        AcceptButton(
+            isAvailable = false,
+            onClick = {
+                authViewModel.saveName(
+                    name = name,
+                )
+                navController.navigate(Screen.PasswordInput.route)
+            },
+        )
+    }
+}
+
+@Composable
+private fun InputNameField(
+    name: String,
+    onValueChange: (String) -> Unit,
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        BasicTextField(
+            value = name,
+            onValueChange = onValueChange,
+            textStyle = TextStyle(
+                fontFamily = SfProText,
+                fontSize = 17.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color.Black
+            ),
+            modifier = Modifier
+                .padding(horizontal = 16.dp),
+            decorationBox = { innerTextField ->
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        if (name.isEmpty()) {
+                            Text(
+                                text = "Введите имя",
+                                fontFamily = SfProText,
+                                fontSize = 17.sp,
+                                color = Color.Gray.copy(alpha = 0.8f)
+                            )
+                        }
+                        innerTextField()
+                    }
+                }
+            }
+        )
+        SeparatorLine(
+            modifier = Modifier
+                .padding(top = 15.dp)
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+private fun AcceptButton(
+    isAvailable: Boolean,
+    onClick: () -> Unit,
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(44.dp)
+            .background(
+                color = if (isAvailable) Blue else Separator,
+                shape = CircleShape
+            )
+            .border(
+                color = if (isAvailable) Blue else Separator,
+                width = 1.dp,
+                shape = CircleShape,
+            )
+            .clip(
+                shape = CircleShape
+            )
+            .clickable(
+                enabled = isAvailable
+            ) {
+                onClick()
+            }
+    ) {
+        Text(
+            text = "Применить",
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Medium,
+            fontSize = 17.sp,
+            color = if (isAvailable) Color.White else Color.Black,
+        )
+    }
+
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun PhoneRegistrationScreenPreview() {
+//    EmailRegistrationScreen()
+}
