051c081daa04c3f6e23da1254c5a91ac8d1b5ece
Danya1910 <shirniy.tonny@gmail.com>
2026-07-16 17:36:56 +0500

    add funcs to observe chats + mine id

--- app/src/main/java/com/example/hydrogram/presentation/screens/ChatListScreen.kt
+++ app/src/main/java/com/example/hydrogram/presentation/screens/ChatListScreen.kt
@@ -2,29 +2,36 @@ package com.example.hydrogram.presentation.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavController
 import com.example.hydrogram.domain.model.Chat
-import com.example.hydrogram.domain.model.User
-import com.example.hydrogram.presentation.states.ChatAndUserUiState
+import com.example.hydrogram.presentation.states.InboxUiState
+import com.example.hydrogram.presentation.viewModel.InboxViewModel
 import com.example.hydrogram.presentation.widgets.BottomBar
 import com.example.hydrogram.presentation.widgets.ChatItem
 import com.example.hydrogram.presentation.widgets.ChatListTopBar
 import com.example.hydrogram.presentation.widgets.SeparatorLine
-import java.util.UUID
 
 
 @Composable
 fun ChatListScreen(
-
+    inboxViewModel: InboxViewModel,
+    navController: NavController,
 ) {
     Scaffold(
         topBar = {
@@ -37,6 +44,8 @@ fun ChatListScreen(
         },
     ) { paddingValues ->
         Content(
+            inboxViewModel = inboxViewModel,
+            navController = navController,
             paddingValues = paddingValues
         )
     }
@@ -44,118 +53,77 @@ fun ChatListScreen(
 
 @Composable
 private fun Content(
+    inboxViewModel: InboxViewModel,
+    navController: NavController,
     paddingValues: PaddingValues,
 ) {
 
-    val currentTimestamp = System.currentTimeMillis()
-    val previewChats = listOf(
-        Chat(
-            senderId = "user_pavel_durov",
-            chatId = UUID.randomUUID().toString(),
-            lastMessage = "Проверь новые обновления безопасности, код отправлен на ревью.",
-            lastMessageTimestamp = currentTimestamp
-        ),
-        Chat(
-            senderId = "user_android_dev",
-            chatId = UUID.randomUUID().toString(),
-            lastMessage = "Я перевел список чатов на LazyColumn! 🚀",
-            lastMessageTimestamp = currentTimestamp - 5 * 60 * 1000,
-            unreadCount = 4
-        ),
-        Chat(
-            senderId = "user_pavel_durov",
-            chatId = UUID.randomUUID().toString(),
-            lastMessage = "Проверь новые обновления безопасности, код отправлен на ревью.",
-            lastMessageTimestamp = currentTimestamp
-        ),
-        Chat(
-            senderId = "user_android_dev",
-            chatId = UUID.randomUUID().toString(),
-            lastMessage = "Я перевел список чатов на LazyColumn! 🚀",
-            lastMessageTimestamp = currentTimestamp - 5 * 60 * 1000,
-            unreadCount = 4
-        ),
-        Chat(
-            senderId = "user_pavel_durov",
-            chatId = UUID.randomUUID().toString(),
-            lastMessage = "Проверь новые обновления безопасности, код отправлен на ревью.",
-            lastMessageTimestamp = currentTimestamp
-        ),
-        Chat(
-            senderId = "user_android_dev",
-            chatId = UUID.randomUUID().toString(),
-            lastMessage = "Я перевел список чатов на LazyColumn! 🚀",
-            lastMessageTimestamp = currentTimestamp - 5 * 60 * 1000,
-            unreadCount = 4
-        ),
-        Chat(
-            senderId = "user_pavel_durov",
-            chatId = UUID.randomUUID().toString(),
-            lastMessage = "Проверь новые обновления безопасности, код отправлен на ревью.",
-            lastMessageTimestamp = currentTimestamp
-        ),
-        Chat(
-            senderId = "user_android_dev",
-            chatId = UUID.randomUUID().toString(),
-            lastMessage = "Я перевел список чатов на LazyColumn! 🚀",
-            lastMessageTimestamp = currentTimestamp - 5 * 60 * 1000,
-            unreadCount = 4
-        ),
-    )
+    val mineId by inboxViewModel.currentId.collectAsStateWithLifecycle()
 
-    // 2. Создаем тестовых пользователей, сопоставленных с ID из чатов
-    val previewUsers = mapOf(
-        "user_pavel_durov" to User(
-            uid = "user_pavel_durov",
-            name = "Павел Дуров",
-            avatarUrl = "https://example.com"
-        ),
-        "user_android_dev" to User(
-            uid = "user_android_dev",
-            name = "Дмитрий (Android)",
-            avatarUrl = ""
-        )
-    )
+    val uiState by inboxViewModel.uiState.collectAsStateWithLifecycle()
 
-    // 3. Маппим (объединяем) их в List<ChatUiState>
-    val uiStates = previewChats.map { chat ->
-        ChatAndUserUiState(
-            chat = chat,
-            user = previewUsers[chat.senderId],
+    LaunchedEffect(Unit) {
+        inboxViewModel.getCurrentUserId()
+    }
+
+    LaunchedEffect(mineId) {
+        inboxViewModel.observeInboxChats(
+            userId = mineId,
         )
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(paddingValues = paddingValues)
-    ) {
-        ChatsList(
-            uiState = uiStates,
-        )
+    when (val state = uiState) {
+        is InboxUiState.Success -> {
+            val chats = state.chats
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues = paddingValues)
+            ) {
+                ChatsList(
+                    chats = chats,
+                    mineId = mineId,
+                    navController = navController,
+                )
+            }
+
+        }
+
+        else -> {
+            // Пока данные конкретного человека грузятся, показываем красивый скелетон-плейсхолдер
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)) {
+                Text(text = "Загрузка...", color = Color.LightGray)
+            }
+        }
     }
 }
 
 
 @Composable
 private fun ChatsList(
-    uiState: List<ChatAndUserUiState>
+    chats: List<Chat>,
+    mineId: String,
+    navController: NavController,
 ) {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
     ) {
         itemsIndexed(
-            items = uiState,
-            key = { _, state -> state.chat.chatId }
-        ) { index, state ->
+            items = chats,
+            key = { _, state -> state.chatId }
+        ) { index, chat ->
 
             ChatItem(
-                chat = state.chat,
-                user = state.user,
+                chat = chat,
+                mineId = mineId,
+                navController = navController,
             )
 
-            if (index != uiState.size - 1) {
+            if (index != chats.size - 1) {
                 SeparatorLine(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -174,6 +142,5 @@ private fun ChatsList(
 @Preview(showBackground = true)
 fun ChatListScreenPreview() {
 
-    ChatListScreen()
 
 }
