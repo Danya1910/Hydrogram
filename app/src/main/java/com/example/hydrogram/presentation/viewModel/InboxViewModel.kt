ee5c5ba29d3d969fc788a5372fcd1db94a248ca3
Danya1910 <shirniy.tonny@gmail.com>
2026-07-16 17:35:26 +0500

    add func to get mine id

--- app/src/main/java/com/example/hydrogram/presentation/viewModel/InboxViewModel.kt
+++ app/src/main/java/com/example/hydrogram/presentation/viewModel/InboxViewModel.kt
@@ -2,30 +2,42 @@ package com.example.hydrogram.presentation.viewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.hydrogram.domain.usecase.GetCurrentUserIdUseCase
 import com.example.hydrogram.domain.usecase.GetInboxChatsUseCase
 import com.example.hydrogram.presentation.states.InboxUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+
+@HiltViewModel
 class InboxViewModel @Inject constructor(
     private val getInboxChatsUseCase: GetInboxChatsUseCase,
-    userId: String,
+    private val getCurrentUserIdUseCase: GetCurrentUserIdUseCase,
 ) : ViewModel()
 {
 
     private val _uiState = MutableStateFlow<InboxUiState>(InboxUiState.Loading)
     val uiState = _uiState.asStateFlow()
 
-    init{
-        observeInboxChats(
-            userId = userId
-        )
+    private val _currentId = MutableStateFlow("")
+    val currentId = _currentId.asStateFlow()
+
+
+    fun getCurrentUserId() {
+        viewModelScope.launch {
+            val result = getCurrentUserIdUseCase()
+            if(!result.isNullOrEmpty()) {
+                _currentId.value = result
+            }
+            else return@launch
+        }
     }
 
-    private fun observeInboxChats(
+    fun observeInboxChats(
         userId: String,
     ) {
         if (userId.isBlank()) {
