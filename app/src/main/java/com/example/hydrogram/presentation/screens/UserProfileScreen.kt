8950abeb0b1584e338e446b122474a038feab6cd
Danya1910 <shirniy.tonny@gmail.com>
2026-07-20 17:24:48 +0500

    add shadow to buttons

--- app/src/main/java/com/example/hydrogram/presentation/screens/UserProfileScreen.kt
+++ app/src/main/java/com/example/hydrogram/presentation/screens/UserProfileScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -256,6 +257,12 @@ private fun GlassButton(
         modifier = Modifier
             .height(44.dp)
             .widthIn(min = 44.dp)
+            .shadow(
+                elevation = 12.dp,
+                shape = CircleShape,
+                clip = true,
+                ambientColor = Color.Black.copy(alpha = 0.9f),
+            )
             .background(
                 brush = GlassBackground,
                 shape = CircleShape
