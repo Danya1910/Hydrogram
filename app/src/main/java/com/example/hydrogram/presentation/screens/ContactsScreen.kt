0b9a9d598d4ae2c7b4ffb2c5b7671804de0d8811
Danya1910 <shirniy.tonny@gmail.com>
2026-07-30 17:32:11 +0500

    fix text field + add close button + correct animation

--- app/src/main/java/com/example/hydrogram/presentation/screens/ContactsScreen.kt
+++ app/src/main/java/com/example/hydrogram/presentation/screens/ContactsScreen.kt
@@ -13,17 +13,24 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -33,6 +40,8 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
@@ -51,6 +60,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
@@ -58,6 +70,7 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
@@ -400,10 +413,12 @@ private fun SearchField(
 ) {
 
     var isTextFieldActive by remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
 
 
     val textFieldAnimation by animateDpAsState(
-        targetValue = if (isTextFieldActive) 38.dp else 0.dp,
+        targetValue = if (isTextFieldActive) 58.dp else 0.dp,
         animationSpec = tween(durationMillis = 300)
     )
 
@@ -417,13 +432,18 @@ private fun SearchField(
         animationSpec = tween(durationMillis = 300)
     )
 
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
+    Box(
+        contentAlignment = Alignment.Center,
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 16.dp)
     ) {
-        Box {
+        Box(
+            modifier = Modifier
+                .height(42.dp)
+                .fillMaxWidth()
+                .padding(end = textFieldAnimation)
+        ) {
             BasicTextField(
                 value = value,
                 onValueChange = onValueChange,
@@ -434,11 +454,8 @@ private fun SearchField(
                     color = Color.Black
                 ),
                 modifier = Modifier
-                    .height(42.dp)
-                    .fillMaxWidth()
-                    .padding(
-                        end = textFieldAnimation,
-                    )
+                    .fillMaxSize()
+                    .focusRequester(focusRequester)
                     .onFocusChanged { focusState ->
                         isTextFieldActive = focusState.isFocused
                     }
@@ -457,14 +474,14 @@ private fun SearchField(
                 decorationBox = { innerTextField ->
                     Box(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight()
+                            .fillMaxSize()
+                            .padding(horizontal = 16.dp) // Внутренний отступ Слой 1
                     ) {
                         Row(
                             verticalAlignment = Alignment.CenterVertically,
                             modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(start = 16.dp, end = 12.dp)
+                                .wrapContentWidth()
+                                .fillMaxHeight()
                                 .align(
                                     BiasAlignment(
                                         horizontalBias = textAlignment,
@@ -478,9 +495,10 @@ private fun SearchField(
                                 tint = Color.Gray.copy(alpha = 0.8f),
                                 modifier = Modifier.size(20.dp),
                             )
+                            Spacer(modifier = Modifier.width(8.dp))
                             Box(
                                 modifier = Modifier
-                                    .weight(1f)
+                                    .wrapContentSize()
                             ) {
                                 if (value.isEmpty()) {
                                     Text(
@@ -500,28 +518,32 @@ private fun SearchField(
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
-                    .height(42.dp)
-                    .fillMaxWidth()
+                    .fillMaxSize()
+                    .graphicsLayer(alpha = textFieldAlpha)
                     .clip(
                         shape = CircleShape,
                     )
-                    .padding(
-                        end = textFieldAnimation,
-                    )
-                    .graphicsLayer(alpha = textFieldAlpha)
                     .background(
-                        color = Color.White,
+                        color = Color(0xFFAEAEB2),
                         shape = CircleShape
                     )
                     .border(
                         width = 1.dp,
                         shape = CircleShape,
-                        color = Color.White,
+                        color = Color(0xFFAEAEB2),
                     )
+                    .clickable(
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() }
+                    ) {
+                        focusRequester.requestFocus()
+                    }
             ) {
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
+                        .wrapContentWidth()
+                        .fillMaxHeight()
                         .align(
                             BiasAlignment(
                                 horizontalBias = textAlignment,
@@ -536,14 +558,67 @@ private fun SearchField(
                         modifier = Modifier.size(20.dp),
                     )
                     Spacer(modifier = Modifier.width(8.dp))
-                    Text(
-                        text = "Поиск",
-                        fontFamily = SfProText,
-                        fontSize = 17.sp,
-                        fontWeight = FontWeight.Normal,
-                        color = Color.Gray.copy(alpha = 0.8f)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .wrapContentSize()
+                    ) {
+                        Text(
+                            text = "Поиск",
+                            fontFamily = SfProText,
+                            fontSize = 17.sp,
+                            fontWeight = FontWeight.Normal,
+                            color = Color.Gray.copy(alpha = 0.8f)
+                        )
+                    }
                 }
+            }
+        }
+        AnimatedVisibility(
+            visible = isTextFieldActive,
+            modifier = Modifier.align(Alignment.CenterEnd),
+            enter = fadeIn(animationSpec = tween(100)) + slideInHorizontally(
+                initialOffsetX = { it },
+                animationSpec = tween(300)
+            ),
+            exit = fadeOut(animationSpec = tween(100)) + slideOutHorizontally(
+                targetOffsetX = { it },
+                animationSpec = tween(300)
+            )
+        ) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .size(42.dp)
+                    .shadow(
+                        elevation = 12.dp,
+                        shape = CircleShape,
+                        clip = false,
+                        ambientColor = Color.Black.copy(alpha = 0.9f),
+                        spotColor = Color.Black.copy(alpha = 0.2f)
+                    )
+                    .clip(
+                        shape = CircleShape,
+                    )
+                    .background(
+                        brush = GlassBackground,
+                        shape = CircleShape,
+                    )
+                    .border(
+                        width = 1.dp,
+                        brush = GlassBorder,
+                        shape = CircleShape,
+                    )
+                    .clickable{
+                        onValueChange("")
+                        focusManager.clearFocus()
+                    }
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_cross),
+                    contentDescription = null,
+                    tint = Color.Unspecified,
+                )
             }
         }
     }
@@ -769,6 +844,15 @@ private fun SortButton(
         modifier = Modifier
             .height(44.dp)
             .width(67.dp)
+            .shadow(
+                elevation = 12.dp,
+                shape = CircleShape,
+                clip = true,
+                ambientColor = Color.Black.copy(alpha = 0.9f),
+            )
+            .clip(
+                shape = CircleShape,
+            )
             .background(
                 brush = GlassBackground,
                 shape = CircleShape,
@@ -818,6 +902,15 @@ private fun AddButton(
         modifier = Modifier
             .height(44.dp)
             .width(44.dp)
+            .shadow(
+                elevation = 12.dp,
+                shape = CircleShape,
+                clip = true,
+                ambientColor = Color.Black.copy(alpha = 0.9f),
+            )
+            .clip(
+                shape = CircleShape,
+            )
             .background(
                 brush = GlassBackground,
                 shape = CircleShape,
