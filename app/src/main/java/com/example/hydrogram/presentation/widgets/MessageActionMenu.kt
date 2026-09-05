4bd7b6aa0f722b328aec2b6fab47d6e543b689d2
Danya1910 <shirniy.tonny@gmail.com>
2026-09-05 21:53:20 +0300

    add emoji + paddings

--- app/src/main/java/com/example/hydrogram/presentation/widgets/MessageActionMenu.kt
+++ app/src/main/java/com/example/hydrogram/presentation/widgets/MessageActionMenu.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
@@ -20,6 +22,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -59,15 +62,21 @@ fun MessageActionMenu(
         "\uD83D\uDC4D",
         "\uD83D\uDC4E",
         "\uD83D\uDD25",
+        "\uD83E\uDD70",
+        "\uD83D\uDE0A",
+        "\uD83D\uDC35",
     )
 
     Column(
-
+        horizontalAlignment = Alignment.End,
+        modifier = Modifier
+            .padding(end = 25.dp)
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(10.dp),
             modifier = Modifier
+                .padding(start = 35.dp, end = 16.dp)
                 .height(47.dp)
                 .clip(
                     shape = CircleShape
@@ -75,7 +84,11 @@ fun MessageActionMenu(
                 .background(
                     brush = rowGradient
                 )
+                .horizontalScroll(rememberScrollState())
                 .padding(horizontal = 10.dp)
+                .align(
+                    alignment = Alignment.Start,
+                )
         ) {
             listOfReactions.forEach { reaction ->
                 Text(
@@ -129,7 +142,7 @@ fun MessageActionMenu(
                             onClick = {
                                 onCopyClick()
                             },
-                            color= LightBlack,
+                            color = LightBlack,
                         )
                     )
                 }
@@ -168,7 +181,7 @@ private fun RowMessageAction(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .height(40.dp)
-            .clickable{
+            .clickable {
                 item.onClick()
             }
             .padding(horizontal = 27.dp)
