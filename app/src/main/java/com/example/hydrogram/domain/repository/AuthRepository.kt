bab2420d08ddd376bf7f134a970cb3d33d19855d
Danya1910 <shirniy.tonny@gmail.com>
2026-09-06 11:48:12 +0300

    add func to check is phone registered

--- app/src/main/java/com/example/hydrogram/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/example/hydrogram/domain/repository/AuthRepository.kt
@@ -2,12 +2,14 @@ package com.example.hydrogram.domain.repository
 
 interface AuthRepository {
 
-    suspend fun signIn(email: String, password: String) : Result<Unit>
+    suspend fun signIn(email: String, password: String): Result<Unit>
 
-    suspend fun signUp(email: String, password: String, name: String, phone: String) : Result<Unit>
+    suspend fun signUp(email: String, password: String, name: String, phone: String): Result<Unit>
 
-    fun getCurrentUserId() : String?
+    suspend fun checkPhoneRegistration(phone: String): Boolean
 
-    fun isUserLoggedIn() : Boolean
+    fun getCurrentUserId(): String?
+
+    fun isUserLoggedIn(): Boolean
 
 }
