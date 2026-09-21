c5296897b1112fefcccac4fc4f875768e967f079
Danya1910 <shirniy.tonny@gmail.com>
2026-09-21 23:07:57 +0300

    feacture: realization of sending image to yandex cloud s3

--- app/src/main/java/com/example/hydrogram/data/repository/StorageRepositoryImpl.kt
+++ app/src/main/java/com/example/hydrogram/data/repository/StorageRepositoryImpl.kt
@@ -2,12 +2,15 @@ package com.example.hydrogram.data.repository
 
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.ObjectMetadata
 import com.amazonaws.services.s3.model.PutObjectRequest
 import com.example.hydrogram.domain.repository.StorageRepository
 import com.example.hydrogram.BuildConfig
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.ByteArrayInputStream
 import java.io.File
+import java.net.URLConnection
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -36,4 +39,33 @@ class StorageRepositoryImpl @Inject constructor(
             return@withContext "https://storage.yandexcloud.net/$bucketName/$s3Key"
 
         }
+
+    override suspend fun uploadAvatar(
+        imageBytes: ByteArray,
+        userId: String,
+        type: String,
+    ): String =
+        withContext(Dispatchers.IO) {
+
+            if (imageBytes.isEmpty()) throw IllegalArgumentException("Массив байт пуст")
+
+            val extension = type.substringAfter("/", "jpg")
+
+            val timestamp = System.currentTimeMillis()
+            val s3Key = "users/$userId/avatar_$timestamp.$extension"
+
+
+            val metadata = ObjectMetadata().apply {
+                contentType = type
+                contentLength = imageBytes.size.toLong()
+            }
+
+            val inputStream = ByteArrayInputStream(imageBytes)
+
+            val request = PutObjectRequest(bucketName, s3Key, inputStream, metadata)
+            s3Client.putObject(request)
+
+            return@withContext "https://storage.yandexcloud.net/$bucketName/$s3Key"
+        }
+
 }
