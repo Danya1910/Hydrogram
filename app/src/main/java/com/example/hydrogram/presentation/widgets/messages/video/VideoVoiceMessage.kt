caca65e91f3458a939e986f1c5cb21132a34088e
Danya1910 <shirniy.tonny@gmail.com>
2026-09-26 19:05:55 +0300

    duration + expand

--- app/src/main/java/com/example/hydrogram/presentation/widgets/messages/video/VideoVoiceMessage.kt
+++ app/src/main/java/com/example/hydrogram/presentation/widgets/messages/video/VideoVoiceMessage.kt
@@ -1,10 +1,15 @@
 package com.example.hydrogram.presentation.widgets.messages.video
 
 import android.text.format.DateFormat
+import android.view.Gravity
+import android.view.TextureView
+import android.widget.FrameLayout
 import androidx.annotation.OptIn
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -14,53 +19,73 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.C
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.ui.AspectRatioFrameLayout
-import androidx.media3.ui.PlayerView
 import com.example.hydrogram.domain.model.Message
 import com.example.hydrogram.ui.theme.DateSeparatorGreen
 import com.example.hydrogram.ui.theme.Green
-import com.example.hydrogram.ui.theme.LightGreen
 import com.example.hydrogram.ui.theme.SfProText
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Date
 
 @Composable
 fun CircleVideoMessage(
     isMine: Boolean,
     message: Message,
+    globalIndex: Int?,
+    lazyListState: LazyListState,
+    bottomPaddingPx: Int,
     onMessageClick: (String) -> Unit,
 ) {
 
-    val videoDuration = DateFormat.format(
-        "HH:mm", Date(message.timestamp)
-    ).toString()
+    val coroutineScope = rememberCoroutineScope()
+
+    var isExpanded by remember { mutableStateOf(false) }
+
+    val widthExpand by animateFloatAsState(
+        targetValue = if (isExpanded) 0.9f else 0.5f,
+    )
+
+    val durationSeconds = (message as Message.CircleVideo).durationSeconds ?: 0
+
+    val minutes = durationSeconds / 60
+    val seconds = durationSeconds % 60
+
+    val videoDuration = String.format("%02d:%02d", minutes, seconds)
 
     val formattedTime = DateFormat.format(
         "HH:mm", Date(message.timestamp)
     ).toString()
 
+    val density = LocalDensity.current
+
     Row(
-        horizontalArrangement = if(isMine) Arrangement.End else Arrangement.Start,
+        horizontalArrangement = if (isMine) Arrangement.End else Arrangement.Start,
         modifier = Modifier
             .fillMaxWidth()
     ) {
@@ -68,14 +93,36 @@ fun CircleVideoMessage(
             contentAlignment = Alignment.Center,
             modifier = Modifier
                 .padding(horizontal = 16.dp)
-                .fillMaxWidth(0.5f)
+                .fillMaxWidth(widthExpand)
                 .aspectRatio(1f)
-                .clickable{
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null
+                ) {
                     onMessageClick(message.messageId)
+                    isExpanded = !isExpanded
+
+                    if (isExpanded && globalIndex != null) {
+                        coroutineScope.launch {
+                            delay(100)
+
+                            val extraMargin = with(density) { 32.dp.roundToPx() }
+                            val scrollOffset = -bottomPaddingPx - extraMargin
+
+                            lazyListState.animateScrollToItem(
+                                index = globalIndex,
+                                scrollOffset = scrollOffset
+                            )
+                        }
+                    }
                 }
         ) {
             CircleVideoPlayer(
-                message = message as Message.CircleVideo,
+                message = message,
+                isExpanded = isExpanded,
+                onCycleEnded = {
+                    isExpanded = false
+                }
             )
             Row(
                 verticalAlignment = Alignment.CenterVertically,
@@ -101,8 +148,9 @@ fun CircleVideoMessage(
 @Composable
 private fun CircleVideoPlayer(
     message: Message.CircleVideo,
+    isExpanded: Boolean,
+    onCycleEnded: () -> Unit,
 ) {
-
     val context = LocalContext.current
 
     val localExoPlayer = remember(message.messageId) {
@@ -111,10 +159,38 @@ private fun CircleVideoPlayer(
             setMediaItem(mediaItem)
             prepare()
 
-            volume = 0f
             repeatMode = Player.REPEAT_MODE_ONE
             playWhenReady = true
         }
+    }
+
+    LaunchedEffect(isExpanded) {
+        if (isExpanded) {
+            localExoPlayer.seekTo(0)
+        }
+    }
+
+    DisposableEffect(localExoPlayer) {
+        val listener = object : Player.Listener {
+            override fun onPositionDiscontinuity(
+                oldPosition: Player.PositionInfo,
+                newPosition: Player.PositionInfo,
+                reason: Int
+            ) {
+                if (reason == Player.DISCONTINUITY_REASON_AUTO_TRANSITION) {
+                    onCycleEnded()
+                }
+            }
+        }
+        localExoPlayer.addListener(listener)
+        onDispose {
+            localExoPlayer.removeListener(listener)
+        }
+    }
+
+    DisposableEffect(isExpanded) {
+        localExoPlayer.volume = if (isExpanded) 1f else 0f
+        onDispose { }
     }
 
     DisposableEffect(message.messageId) {
@@ -126,34 +202,31 @@ private fun CircleVideoPlayer(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .clip(
-                shape = CircleShape,
-            )
-
-            .border(
-                width = 1.dp,
-                color = Green,
-            )
+            .clip(CircleShape)
+            .border(width = 1.dp, color = Green, shape = CircleShape)
     ) {
         AndroidView(
-            factory = { context ->
-                PlayerView(context).apply {
-                    useController = false
+            factory = { ctx ->
+                TextureView(ctx).apply {
+                    layoutParams = FrameLayout.LayoutParams(
+                        FrameLayout.LayoutParams.MATCH_PARENT,
+                        FrameLayout.LayoutParams.MATCH_PARENT,
+                        Gravity.CENTER
+                    )
 
-                    resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
-
-                    player = localExoPlayer
+                    localExoPlayer.setVideoTextureView(this)
                 }
             },
-            update = { playerView ->
-                if(playerView.player != localExoPlayer) {
-                    playerView.player = localExoPlayer
-                }
+            update = { textureView ->
+                localExoPlayer.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
+
+                localExoPlayer.setVideoTextureView(textureView)
             },
             modifier = Modifier.fillMaxSize()
         )
     }
 }
+
 
 @Composable
 private fun VideoInfo(
