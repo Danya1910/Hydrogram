fad4b4524a1c2571b9702946b732bd2d39691209
Danya1910 <shirniy.tonny@gmail.com>
2026-09-26 12:50:12 +0300

    refactor

--- app/src/main/java/com/example/hydrogram/presentation/widgets/messages/video/VideoVoiceMessage.kt
+++ app/src/main/java/com/example/hydrogram/presentation/widgets/messages/video/VideoVoiceMessage.kt
@@ -1,14 +1,12 @@
-package com.example.hydrogram.presentation.widgets.video
+package com.example.hydrogram.presentation.widgets.messages.video
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.anchoredDraggable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,8 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.fromColorLong
-import androidx.compose.ui.layout.ScaleFactor
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
